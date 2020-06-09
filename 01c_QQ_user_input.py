d8af1523abbae087adc10ba7f7aa7235896fc815
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2020-06-09 14:50:41 +1200

    Current

--- 01c_QQ_user_input.py
+++ 01c_QQ_user_input.py
@@ -3,10 +3,12 @@
 # String Checker
 dif_range = ["hard", "normal", "easy"]
 
-def str_checker(question, to_check):
+
+def str_checker(question, to_check, error):
     valid = False
     while not valid:
 
+        # Ask question
         response = input(question).lower()
 
         for item in to_check:
@@ -14,27 +16,16 @@ def str_checker(question, to_check):
                 return response
             elif response == item[0]:
                 return item
-        error = print_list(to_check, len(to_check))
-        print()
+        print("Please enter either {}".format(error))
 
-def print_list(list, length):
-    statement = ""
-    for i in range(0, length):
-        if i == 0:
-            statement = statement + " " + list[i]
-        elif i == 1:
-            statement = statement + ", " + list[i]
-        else:
-            statement = statement + ", {}.".format(list[i])
-    print("Sorry, please enter one of{}".format(statement))
-
-difficulty = str_checker("Difficulty (Easy, Normal or Hard): ",dif_range).lower()
+# Set all the vaiables when do
+difficulty = str_checker("Difficulty (Easy, Normal or Hard): ",dif_range, "Hard, Normal or Easy").lower()
 
 if difficulty == "easy" or difficulty == "e":
     high = 10
 elif difficulty == "normal" or difficulty == "n":
     high = 50
-else:
+elif difficulty == "hard" or difficulty == "h":
     high = 100
 
 print(high)
